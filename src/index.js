94da1804811677d428b6159a244771c1fe77e02c
ONYX <krishnasahnionyx@gmail.com>
2026-09-01 13:44:43 +0530

    add user and video model

--- src/index.js
+++ src/index.js
@@ -1,9 +1,25 @@
 //require('dotenv').config({path: './env'})
 import dotenv from "dotenv"
 import connectDB from "./db/index.js";
+import { app } from "./app.js"
 
 dotenv.config({
     path: './.env'
 })
 
-connectDB()
+;(async () => {
+    try{
+        await connectDB()
+
+        const port = process.env.PORT || 8000
+        app.on("error", (error) => {
+            console.log("EXPRESS ERROR: ", error)
+            throw error
+        })
+        app.listen(port, () => {
+            console.log(`Server is running on port: ${port}`)
+        })
+    }catch(error) {
+        console.log("MONGODB connection failed !!!", error)
+    }
+})()
