11b5a8147dc6af403e06b160624f2cedb2336a3f
0NYX07 <krishnasahnionyx@gmail.com>
2026-09-23 14:26:58 +0530

    created frontend and started connection to backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import heroImg from './assets/hero.png'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import './App.css'
+import axios from 'axios'
 
 function App() {
   const [count, setCount] = useState(0)
